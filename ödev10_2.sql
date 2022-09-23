bf9ae64c146ecee0aac7c63568c8c3dc4a0c03a0
muzeyyenkoksal06 <112731250+muzeyyenkoksal06@users.noreply.github.com>
2022-09-23 18:01:04 +0300

    Update ödev10_2.sql

--- ödev10_2.sql
+++ ödev10_2.sql
@@ -1,2 +1,2 @@
 SELECT customer.first_name, customer.last_name,payment.payment_id FROM customer 
-LEFT JOIN payment ON customer.customer_id = payment.customer_id;
+RIGHT JOIN payment ON customer.customer_id = payment.customer_id;
